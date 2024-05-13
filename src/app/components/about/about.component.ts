85582891a053ea3e3c9f3e283b5f920437c5b652
root <root@localhost.localdomain>
2024-05-13 01:20:05 -0500

    aplicaciongarente

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  constructor(
+    public global: GlobalService
+  ){}
+
 
 }
